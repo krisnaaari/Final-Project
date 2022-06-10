f78e6805bb9f2a0b27171b88b17209b24a368285
wisnuwdn <91248099+wisnuwdn@users.noreply.github.com>
2022-06-11 04:53:50 +0800

    UI Satu + function view + cetak tiket
    Sejauh ini baru Create file doang. Fungsionalitas untuk Read, Update, Delete gak yakin mau dicantumin di program karena kalo udh beli tiket ga mungkin bisa diubah tempat duduk, jadwal, cinema, dsb.

--- UI Satu.c
+++ UI Satu.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #define MAXTICKET 10
 
 typedef struct tiket_t{
@@ -12,7 +13,7 @@ typedef struct tiket_t{
     char* jadwal;
 } tiket;
 
-tiket* ticketList[MAXTICKET];
+tiket ticketList[MAXTICKET];
 
 char bioskop[5][25]={"Beach Walk", "Galeria", "Level 21", "Park 23", "TSM"};
 char film[5][20]={"Avanger", "Transformer", "Doctor Strange", "Spiderman", "Susah Sinyal"};
@@ -23,11 +24,12 @@ void UI1();
 void PilihBioskopFilm(); //function pilih bioskop + film
 void topUpSaldo();
 void myTicket();
-void cetakTiket();
+void cetakTiket(int index);
 void ticketInit();
 
-void main(){    
+int main(){  
     UI1();
+    return 0;
 }
 
 
@@ -49,14 +51,16 @@ void UI1(){
                 topUpSaldo();
                 break;
             case 3:
-                printf("test");
+                myTicket();
+                system("pause");
+                fflush(stdin);
                 break;
             case 4:
                 exit(0);
             default:
                 printf("Input tidak valid!");
                 printf("Ulangi inputan!");
-                _sleep(2);
+                system("pause");
         }
     }
 }
@@ -143,35 +147,88 @@ void topUpSaldo(){
 
 void ticketInit(){
     for (int i = 0; i < MAXTICKET; i++)
-        ticketList[i] = NULL;
+        ticketList[i].nama = NULL;
 }
 
 void myTicket(){
     char userInput;
     char* id;
+    int urutan;
     ticketInit();
     while (true){
         for (int i = 0; i < MAXTICKET; i++){
-            if (ticketList[i] == NULL) return;
+            if (ticketList[i].nama == NULL) continue;
             //cetak tiketnya
+            printf("tiket %d\n", i+1);
+            printf("bioskop: %s\n", ticketList[i].cinema);
+            printf("film   : %s\n", ticketList[i].film);
+            printf("jadwal : %s\n", ticketList[i].jadwal);
+            printf("kursi  : %s\n", ticketList[i].kursi);
+            printf("ID     : %s\n\n", ticketList[i].ticketID);
         }
-        printf("Cetak tiket? y/n");
+        if (ticketList[0].nama == NULL){
+            printf("Anda belum memiliki tiket!\n");
+            return;
+        }
+        fflush(stdin);
+        printf("Cetak tiket? y/n\n");
         scanf("%c", &userInput);
         if (userInput == 'y'){
-            printf("masukkan id tiket: ");
-            scanf("%s", &id);
-            //call function cetak tiket
+            while (true){
+                printf("Nomor urutan tiket yang ingin dicetak: ");
+                scanf("%d", &urutan);
+                if (urutan < 1 || urutan > 10 || ticketList[urutan-1].nama == NULL){
+                    printf("Masukkan angka yang valid!");
+                }
+                else break;
+            }
+            cetakTiket(urutan-1);
+            printf("\nTiket tercetak. Periksa text file!\n");
             return;
         }
-        else if (userInput == 'n'){
-            return;
-        }
-        else{
-            printf("salah input, ulangi!");
-        }
+        else if (userInput == 'n') return;
+        else printf("salah input, ulangi!");
     }
 }
 
 void cetakTiket(int index){
-    //create txt file and open file
+    int i;
+    char* tmp;
+    tiket tix = ticketList[index];
+    FILE *fp;
+    fp = fopen("file.txt", "r+");
+    if (fp == NULL){
+        printf("Cannot open file!");
+        system("pause");
+        fflush(stdin);
+        return;
+    }
+    //WRITE FILE
+    //bioskop
+    fputs("==== ", fp);
+    for (i = 0; i < strlen(tix.cinema); i++){
+        fputc(tix.cinema[i], fp);
+    }
+    fputs(" ====\n", fp);
+    //film
+    fputs("\n", fp);
+    for (i = 0; i < strlen(tix.film); i++) fputc(tix.film[i], fp);
+
+    //jadwal
+    fputs("\n", fp);
+    for (i = 0; i < strlen(tix.jadwal); i++) fputc(tix.jadwal[i], fp);
+
+    //kursi
+    fputs("\nSeat: ", fp);
+    for (i = 0; i < strlen(tix.kursi); i++) fputc(tix.kursi[i], fp);
+    
+    // nama
+    fputs("\n", fp);
+    for (i = 0; i < strlen(tix.nama); i++) fputc(tix.nama[i], fp);
+    
+    //ticket id
+    fputs("\nTicket ID: ", fp);
+    for (i = 0; i < strlen(tix.ticketID); i++) fputc(tix.ticketID[i], fp);
+
+    fclose(fp);
 }
